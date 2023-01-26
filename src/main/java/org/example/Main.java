e385bded94a101e688e6d8b4fa93e493a1c077b7
valfink <valentin.finke@gmail.com>
2023-01-26 12:41:52 +0100

    More lines

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello again!");
         System.out.println("Hello hello!");
+        System.out.println("Hello again!");
+        System.out.println("Hello again!");
     }
 }
